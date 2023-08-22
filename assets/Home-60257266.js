2d999022a4966af0f04be128fcda1c8a30322eb1
livegp <livegp@users.noreply.github.com>
2023-08-22 16:11:37 +0000

    deploy: 8893bbc3087d36c272b8edb23468e23a102ea151

--- assets/Home-60257266.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-8829ef19.js";import{u as s,G as i}from"./useDataFetching-432d7785.js";import"./Loader-710b74d4.js";import"./react-toastify.esm-7a21454c.js";import"./fetch-5d12fe7f.js";function c(){const t="In trend",{results:o,loading:r,total:e,handleLoadMore:n}=s("trending","",1);return a.jsx(i,{results:o,title:t,loading:r,onLoadMore:n,total:e})}export{c as default};
